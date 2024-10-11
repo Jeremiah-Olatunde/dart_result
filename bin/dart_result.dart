b34238970361966b8ceac5195db3e63658404f5c
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-11 21:59:27 +0100

    chore(setup): maiden commit

---
+++ bin/dart_result.dart
@@ -0,0 +1,5 @@
+import 'package:dart_result/dart_result.dart' as dart_result;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_result.calculate()}!');
+}
---
+++ test/dart_result_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_result/dart_result.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
