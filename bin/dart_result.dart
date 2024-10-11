975e5b3ad7cb9df1c2a2675f206eddee09c6795d
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-11 22:03:15 +0100

    chore(setup): add initial setup and README

--- bin/dart_result.dart
+++ bin/dart_result.dart
@@ -1,5 +1,6 @@
 import 'package:dart_result/dart_result.dart' as dart_result;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_result.calculate()}!');
+  print('hello result');
+  print('-------------------------------------');
 }
