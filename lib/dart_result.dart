f8294bd5c04cb7d687134519a0e757b6703d7afe
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-11 22:08:00 +0100

    feat: add classes Result, Ok and Err

--- lib/dart_result.dart
+++ lib/dart_result.dart
@@ -1,1 +1,11 @@
+sealed class Result<T, E> {}
 
+class Ok<T, E> implements Result<T, E> {
+  final T value;
+  const Ok(this.value);
+}
+
+class Err<T, E> implements Result<T, E> {
+  final E error;
+  const Err(this.error);
+}
